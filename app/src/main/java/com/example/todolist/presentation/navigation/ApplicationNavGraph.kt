f0cc0f93fe898f589843b3896ebc36fddbd51951
valenty`an <valentindremin675@gmail.com>
2026-03-09 14:10:09 +0300

    navHost

--- app/src/main/java/com/example/todolist/presentation/navigation/ApplicationNavGraph.kt
+++ app/src/main/java/com/example/todolist/presentation/navigation/ApplicationNavGraph.kt
@@ -7,6 +7,10 @@ import androidx.compose.ui.graphics.Color
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
+import com.example.todolist.presentation.screens.TodoDetailScreen
+import com.example.todolist.presentation.screens.TodoListScreen
 
 import com.example.todolist.presentation.viewmodel.TodoViewModel
 
@@ -18,11 +22,26 @@ fun ApplicationNavGraph(navController: NavHostController, viewModel: TodoViewMod
         modifier = Modifier.background(Color(0xFFFFBDBD))
         ){
         composable(route = "TodoList"){
-
+            TodoListScreen(
+                todoTasks = viewModel.todos,
+                onTodoClick = {id -> navController.navigate(route = "Detail/{$id}")},
+                onToggleClick = {id -> viewModel.toggleTodo(id)}
+            )
         }
 
-        composable(route = "Detail"){
+        composable(
+            route = "Detail/{todoId}",
+            arguments = listOf(navArgument("todoId") { type = NavType.IntType })
+            ){ backStackEntry ->
+            val todoId = backStackEntry.arguments?.getInt("todoId") ?: return@composable
+            val todo = viewModel.todos.value.find{it.id == todoId}
 
+            todo?.let {
+                TodoDetailScreen(
+                    todo = it,
+                    onBackClick = {navController.popBackStack()}
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/todolist/presentation/viewmodel/TodoViewModel.kt
+++ app/src/main/java/com/example/todolist/presentation/viewmodel/TodoViewModel.kt
@@ -1,14 +1,37 @@
 package com.example.todolist.presentation.viewmodel
 
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.todolist.domain.model.TodoItem
 import com.example.todolist.domain.usecase.GetTodosUseCase
 import com.example.todolist.domain.usecase.ToggleTodoUseCase
-import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 class TodoViewModel(
     private val getTodos: GetTodosUseCase,
     private val toggleTodos: ToggleTodoUseCase
 ) : ViewModel() {
-    //
+    private val statusTodos = mutableStateOf<List<TodoItem>>(emptyList())
+    val todos: State<List<TodoItem>> = statusTodos
+
+    init {
+        loadTodos()
+    }
+
+    fun loadTodos() {
+        viewModelScope.launch {
+            statusTodos.value = getTodos()
+        }
+    }
+
+    fun toggleTodo(id: Int) {
+        viewModelScope.launch {
+            toggleTodos(id)
+            statusTodos.value = getTodos()
+        }
+    }
+
+    fun getTodoById(id: Int): TodoItem? = statusTodos.value.firstOrNull { it.id == id }
 }
